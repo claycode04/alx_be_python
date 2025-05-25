030d1be31ddc6eb37c8503fd554ff3872b126557
claycode04 <yassdev4@gmail.com>
2025-05-25 19:49:53 +0100

    Add basic arithmetic operations: addition, subtraction, and multiplication

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+num1 = 10
+num2 = 5
+
+addition = num1 + num2
+print("Addition of 10 and 5 is", addition)
+subtraction = num1 - num2
+print("Subtraction of 10 and 5 is", subtraction)
+multiplication = num1 * num2
+print("Multiplication of 10 and 5 is", multiplication)
