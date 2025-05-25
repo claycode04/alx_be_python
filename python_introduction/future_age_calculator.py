845257075c4dc78a16a78bad90ff3966fe1a5dda
claycode04 <yassdev4@gmail.com>
2025-05-25 20:12:54 +0100

    Add scripts for future age calculation, hours to seconds conversion, and rectangle area calculation

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,2 @@
+input_age=int(input("Enter your current age: "))
+print("In 2050 years, you will be", input_age + 27, "years old.")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,2 @@
+hours = 2
+print(f"{hours} hours is {hours * 3600} seconds")
